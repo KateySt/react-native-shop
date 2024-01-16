1ebc81703649f2bf5880b9e03597063cf2625dc9
KateSt <kateystetsenko@gmail.com>
2024-01-16 19:54:47 +0200

    init

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,7 +1,19 @@
 import createExpoWebpackConfigAsync from '@expo/webpack-config/webpack';
-import { Arguments, Environment } from '@expo/webpack-config/webpack/types';
+import {Arguments, Environment} from '@expo/webpack-config/webpack/types';
 
 module.exports = async function (env: Environment, argv: Arguments) {
     const config = await createExpoWebpackConfigAsync(env, argv);
+    if (config.resolve?.alias) {
+        config.resolve.alias = {
+            ...config.resolve.alias,
+            '@/*': './src/*',
+            '@/api': './src/api',
+            '@/components': './src/components',
+            '@/utils': './src/utils',
+            '@/screens': './src/screens',
+            '@/navigation': './src/navigation',
+            '@/constants': './src/constants',
+        }
+    }
     return config;
 };
